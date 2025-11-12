378c919faf7ad01d4d379dc0a8abf5e5d965729a
Mayur Raut <95993675+RazoBeckett@users.noreply.github.com>
2025-11-12 11:47:24 +0530

    feat(session): Configure session cookie cache
    Enable cookie caching for user sessions.
    Set a maximum age of 5 minutes for cached session cookies.
    This enhances session management by leveraging cookie caching.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,6 +6,12 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+  session: {
+    cookieCache: {
+      enabled: true,
+      maxAge: 60 * 5, // 5 mintues
+    },
+  },
   database: drizzleAdapter(db, {
     provider: "pg",
   }),
